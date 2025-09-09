f21076811e4b3a6ddbd48e9e7504c070df5a04c6
rainingcodes <rainingcodes0@gmail.com>
2025-09-09 10:02:19 +0900

    update baseline.py

--- src/baseline.py
+++ src/baseline.py
@@ -3,7 +3,7 @@ import numpy as np
 
 from sklearn.ensemble import RandomForestClassifier
 
-train = pd.read_csv('../data/train.csv')
+train = pd.read_csv('data/train.csv')
 
 train_x = train.drop(columns=['ID', 'target'])
 train_y = train['target']
@@ -11,14 +11,14 @@ train_y = train['target']
 model = RandomForestClassifier(random_state=42)
 model.fit(train_x, train_y) 
 
-test = pd.read_csv('../data/test.csv')
+test = pd.read_csv('data/test.csv')
 
 test_x = test.drop(columns=['ID'])
 preds = model.predict(test_x)
 
-submission = pd.read_csv('../data/sample_submission.csv')
+submission = pd.read_csv('data/sample_submission.csv')
 
 submission['target'] = preds
 submission
 
-submission.to_csv('../data/baseline_submit.csv', index=False, encoding='utf-8-sig')
+submission.to_csv('data/baseline_submit.csv', index=False, encoding='utf-8-sig')
